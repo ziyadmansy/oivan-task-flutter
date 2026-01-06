fed3eaefe7c3d1c889a8af09663f4aae50d2f24f
Ziyad Mansy <ziyadmohammad37@gmail.com>
2026-01-06 20:57:29 +0200

    feat: Update application ID and namespace, adjust build configurations, and refactor MainActivity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.inovola_task"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.example.oivan_task"
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -18,13 +18,9 @@ android {
     kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID
-        // (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.inovola_task"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        applicationId = "com.example.oivan_task"
+        minSdk = 26
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
